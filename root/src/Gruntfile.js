bf2bd3f0a7d0f47240d1c4359e87e013b710afe9
plato-cambrian <plato@cambrian.org>
2014-09-18 06:17:34 -0600

    Add sass and templated gruntfile

---
+++ root/src/Gruntfile.js
@@ -0,0 +1,94 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    {% 
+    if(autocompile){
+    %}
+      watch: {
+        jade: {
+          files: ['jade/*'],
+          tasks: ['jade:compile']
+        },
+        sass: {
+          files: ['sass/*'],
+          tasks: ['sass:compile']
+        }
+      },
+    {%
+    };
+    %}
+    bower: {
+      install: {
+        options:{
+          targetDir: '../component/bower_components',
+          layout: 'byComponent',
+          copy: true,
+          cleanTargetDir: true,
+          verbose: true,
+        }
+      },
+    },
+    jade: {
+      compile: {
+        options: {client: false, pretty: true},
+        files: [
+          {
+            cwd: 'jade',
+            src: '*.jade',
+            dest: '../component',
+            expand: true,
+            ext: '.html'
+          },
+        ],
+      },
+    },
+    sass: {
+      compile: {
+        options: {
+          style: "expanded",
+          pretty: true
+        },
+        files: [
+          {
+            cwd: 'sass',
+            src: '*.sass',
+            dest: '../component',
+            expand: true,
+            ext: '.css'
+          },
+        ],
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-bower-task');
+  {% 
+  if(autocompile){
+  %}
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+  {%
+  };
+  %}
+
+  // Default task.
+  grunt.registerTask('default', [
+    'bower:install',
+    {% 
+    if(autocompile){
+    %}
+      'jade:compile',
+      'sass:compile',
+      'watch:jade'
+      'watch:sass'
+    {%
+    };
+    %}
+  ]);
+
+};
